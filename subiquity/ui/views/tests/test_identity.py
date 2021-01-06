350c752a46456c8b478a752ea44001fc4c551034
Julien Rhodes <julien.rhodes@planet.com>
2021-01-06 10:19:16 -0800

    fix unittests, add unittest for identityhostname installer page

--- subiquity/ui/views/tests/test_identity.py
+++ subiquity/ui/views/tests/test_identity.py
@@ -4,18 +4,22 @@ from unittest import mock
 from subiquitycore.signals import Signal
 from subiquitycore.testing import view_helpers
 
-from subiquity.client.controllers.identity import IdentityController
-from subiquity.common.types import IdentityData
+from subiquity.controllers.identity import IdentityController, IdentityHostnameController
+from subiquity.common.types import IdentityData, IdentityHostnameData
 from subiquity.ui.views.identity import IdentityView
+from subiquity.ui.views.identity import IdentityView, IdentityHostnameView
 
 
-valid_data = {
+identity_data = {
     'realname': 'Real Name',
-    'hostname': 'host-name',
     'username': 'username',
     'password': 'password',
     'confirm_password': 'password'
     }
+
+identityhostname_data = {
+    'hostname': 'ubuntu-server'
+}
 
 
 class IdentityViewTests(unittest.TestCase):
@@ -40,7 +44,7 @@ class IdentityViewTests(unittest.TestCase):
 
     def test_done_enabled_when_valid(self):
         view = self.make_view()
-        view_helpers.enter_data(view.form, valid_data)
+        view_helpers.enter_data(view.form, identity_data)
         self.assertTrue(view.form.done_btn.enabled)
 
     def test_click_done(self):
@@ -54,7 +58,6 @@ class IdentityViewTests(unittest.TestCase):
         expected = IdentityData(
             realname=valid_data['realname'],
             username=valid_data['username'],
-            hostname=valid_data['hostname'],
             crypted_password=CRYPTED)
         view.controller.done.assert_called_once_with(expected)
 
@@ -67,7 +70,53 @@ class IdentityViewTests(unittest.TestCase):
         # by simulating lots of presses of the tab key and checking if
         # the done button has been focused.
         view = self.make_view()
-        view_helpers.enter_data(view.form, valid_data)
+        view_helpers.enter_data(view.form, identity_data)
+
+        for i in range(100):
+            view_helpers.keypress(view, 'tab', size=(80, 24))
+            focus_path = view_helpers.get_focus_path(view)
+            for w in reversed(focus_path):
+                if w is view.form.done_btn:
+                    return
+        self.fail("could not tab to done button")
+
+
+class IdentityHostnameViewTests(unittest.TestCase):
+
+    def make_view(self):
+        model = mock.create_autospec(spec=IdentityHostnameModel)
+        controller = mock.create_autospec(spec=IdentityHostnameController)
+        controller.signal = mock.create_autospec(spec=Signal)
+        return IdentityHostnameView(model, controller)
+
+    def test_done_initially_disabled(self):
+        view = self.make_view()
+        self.assertFalse(view.form.done_btn.enabled)
+
+    def test_done_enabled_when_valid(self):
+        view = self.make_view()
+        view_helpers.enter_data(view.form, identityhostname_data)
+        self.assertTrue(view.form.done_btn.enabled)
+
+    def test_click_done(self):
+        view = self.make_view()
+        view_helpers.enter_data(view.form, identityhostname_data)
+
+        done_btn = view_helpers.find_button_matching(view, "^Done$")
+        view_helpers.click(done_btn)
+
+        view.controller.done.assert_called_once_with(identityhostname_data)
+
+    def test_can_tab_to_done_when_valid(self):
+        # Urwid doesn't distinguish very well between widgets that are
+        # not currently selectable and widgets that can never be
+        # selectable. The "button pile" of the identity view is
+        # initially not selectable but becomes selectable when valid
+        # data is entered. This test checks that urwid notices this :)
+        # by simulating lots of presses of the tab key and checking if
+        # the done button has been focused.
+        view = self.make_view()
+        view_helpers.enter_data(view.form, identityhostname_data)
 
         for i in range(100):
             view_helpers.keypress(view, 'tab', size=(80, 24))
